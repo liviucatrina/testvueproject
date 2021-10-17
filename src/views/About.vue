51fb9ab34ac46317e0e0bc8ff15c95cbdee171c4
liviucatrina <liviucatrina@gmail.com>
2021-10-17 12:38:58 +0300

    Fix text content horizontal alignment

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="d-flex justify-content-center">
-    <h1>Test Vue JS</h1>
-    <p>Test Jenkins/Docker flow</p>
+    <div class="text-center">
+      <h1>Test Vue JS</h1>
+      <p>Test Jenkins/Docker flow</p>
+    </div>
   </div>
 </template>
